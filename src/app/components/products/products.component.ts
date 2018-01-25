d0e0875bd8cb9c2d83a76fc832df633c26f554ca
kimshuye <suntisuk.go@gmail.com>
2018-01-26 00:07:59 +0700

    connect firebase Database ok! but output console only

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+
 
 @Component({
   selector: 'app-products',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-
-  constructor() { }
+  public products: any[];
+  
+  constructor(private db: AngularFireDatabase) { 
+    db.list<Product>('/products').valueChanges().subscribe(
+      products => {
+        this.products = products;
+        console.log(this.products);
+      }
+    );
+    
+  }
 
   ngOnInit() {
   }
 
+}
+
+interface Product{
+  $name?: string;
+  description?: string;
+  $sku?: string;
+  sellprice?: string;
+  purchaseprice?: string;
+  stock?: string;
+  availablestock?: string;
+  unittext?: string;
 }
